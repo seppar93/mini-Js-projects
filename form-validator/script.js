154b5bbc78300fa4b755eee54ab6dda916d2d1bd
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-08-08 15:55:04 -0400

    confirm passwords

--- form-validator/script.js
+++ form-validator/script.js
@@ -8,6 +8,10 @@ const confrimPassword = document.getElementById("confirmPassword");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkRequires([userName, email, password, confrimPassword]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  isValidEmail(email.input);
+  checkPasswordsMatch(password,confrimPassword)
 
   //   if (username.value === "") {
   //     showError(username, "Username is required");
@@ -56,11 +60,17 @@ function getFieldName(input) {
 
 // check password
 function isValidEmail(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+  if (
+    String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+  ) {
+    showSuccess(email);
+  } else {
+    showError(email, "email is not rather");
+  }
 }
 
 // show input error message
@@ -75,4 +85,26 @@ function showError(input, message) {
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
+}
+
+// check input length
+
+function checkLength(input, min, max) {
+  if (input.value.legnth < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  }
+}
+
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, "passwords do not match")
+    }
 }
